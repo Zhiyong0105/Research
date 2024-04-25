5f32d1de4c468c9681c5d70b8de042c0e8ca2c2d
Zhiyong0105 <edios4829@gmail.com>
2024-04-25 16:30:48 +0900

    modified

--- code/analysis.py
+++ code/analysis.py
@@ -2,6 +2,20 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
 
+k_v = [24,96,192]
 data = pd.read_csv("result.txt", sep=" ", header=None, names=["name","n","k","gflops","time","useless"])
 data.to_csv("data.csv",index=False)
-groupmean = data.groupby(["name","n","k"]).mean()
+Groupmean_data = pd.read_csv("data.csv")
+# print(Groupmean_data)
+for k in k_v:
+    plt.figure(figsize=(7, 5))
+    for idx,name in enumerate(Groupmean_data["name"].unique()):
+        #print(name)
+        sub = Groupmean_data[(Groupmean_data["name"]==name)&(Groupmean_data["k"]==k)]
+        # print(sub)
+        # subset = Groupmean_data[(Groupmean_data["name"]==name)&(Groupmean_data["k"]==k)]
+        # print(subset)
+        plt.plot(sub['n'], sub['gflops'], label=name, color="red", linestyle="--",marker='o')
+        plt.tight_layout()
+        plt.savefig("G_husing_{}.pdf".format(k))
+        plt.close()
